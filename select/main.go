6d83233790e74cbb4d4f772c64474266ba125d9c
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-06-24 23:52:00 +0200

    Using time.After with anther package.

--- select/main.go
+++ select/main.go
@@ -2,31 +2,47 @@ package main
 
 import (
 	"fmt"
+	"select/database"
 	"time"
 )
 
-func message(c chan string, s string) {
-	for {
-		time.Sleep(1 * time.Second)
-		c <- s
-	}
-}
+// func message(c chan string, s string) {
+// 	for {
+// 		time.Sleep(1 * time.Second)
+// 		c <- s
+// 	}
+// }
 
 func main() {
-	chan1 := make(chan string)
-	chan2 := make(chan string)
+	channel := make(chan bool)
 
-	go message(chan1, "hi")
-	go message(chan2, ":)")
+	go database.Save("hi :)", channel)
 
 	for {
 		select {
-		case msg1 := <-chan1:
-			fmt.Println(msg1)
-		case msg2 := <-chan2:
-			fmt.Println(msg2)
-		default:
-			fmt.Println("Default")
+		case <-channel:
+			fmt.Println("Saved")
+			return
+		case <-time.After(3 * time.Second):
+			fmt.Println("This is taking too long :(")
+			return
 		}
 	}
+
+	// chan1 := make(chan string)
+	// chan2 := make(chan string)
+
+	// go message(chan1, "hi")
+	// go message(chan2, ":)")
+
+	// for {
+	// 	select {
+	// 	case msg1 := <-chan1:
+	// 		fmt.Println(msg1)
+	// 	case msg2 := <-chan2:
+	// 		fmt.Println(msg2)
+	// 	default:
+	// 		fmt.Println("Default")
+	// 	}
+	// }
 }
