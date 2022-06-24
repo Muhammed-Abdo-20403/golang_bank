45875feb710e7e3e3d9dc9ed0b35e021628a5833
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-06-25 00:15:49 +0200

    Using  "time.NewTicker"

--- select/main.go
+++ select/main.go
@@ -12,10 +12,24 @@ import (
 // 	}
 // }
 
-func message(c chan string, s string) {
+// func message(c chan string, s string) {
+// 	for {
+// 		time.Sleep(500 * time.Millisecond)
+// 		c <- s
+// 	}
+// }
+
+func echo(done <-chan bool, interval time.Duration) {
+	ticker := time.NewTicker(interval)
 	for {
-		time.Sleep(500 * time.Millisecond)
-		c <- s
+		select {
+		case t := <-ticker.C:
+			fmt.Println("Ticker tick ", t.Second())
+			// get data from API
+		case <-done:
+			ticker.Stop()
+			return
+		}
 	}
 }
 
@@ -52,24 +66,24 @@ func main() {
 	// 	}
 	// }
 
-	chan1 := make(chan string)
-	chan2 := make(chan string)
+	// chan1 := make(chan string)
+	// chan2 := make(chan string)
 
-	go message(chan1, "hi")
-	go message(chan2, ":)")
+	// go message(chan1, "hi")
+	// go message(chan2, ":)")
 
-	timer := time.NewTimer(2 * time.Second)
-	for {
-		select {
-		case msg1 := <-chan1:
-			fmt.Println(msg1)
-		case msg2 := <-chan2:
-			fmt.Println(msg2)
-		case <-timer.C:
-			fmt.Println("Timer ended")
-			return
-		}
-	}
+	// timer := time.NewTimer(2 * time.Second)
+	// for {
+	// 	select {
+	// 	case msg1 := <-chan1:
+	// 		fmt.Println(msg1)
+	// 	case msg2 := <-chan2:
+	// 		fmt.Println(msg2)
+	// 	case <-timer.C:
+	// 		fmt.Println("Timer ended")
+	// 		return
+	// 	}
+	// }
 
 	// timer := time.NewTimer(2 * time.Second)
 	// <-timer.C
@@ -86,4 +100,24 @@ func main() {
 	// 	fmt.Println("Timer stopped")
 	// }
 	// time.Sleep(3 * time.Second)
+
+	// ticker := time.NewTicker(1 * time.Second)
+	// timer := time.NewTimer(5 * time.Second)
+
+	// for {
+	// 	select {
+	// 	case t := <-ticker.C:
+	// 		fmt.Println("Ticker tick", t.Second())
+	// 	case <-timer.C:
+	// 		ticker.Stop()
+	// 		return
+	// 	}
+
+	// }
+	done := make(chan bool)
+	go echo(done, time.Second*1)
+
+	time.Sleep(5 * time.Second)
+	done <- true
+	fmt.Println("Ticker stopped")
 }
