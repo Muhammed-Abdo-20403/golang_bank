119e967696c42f8c4f528cb249e725505aa60df3
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-06-26 15:30:42 +0200

    reading JSON file from "files" folder

--- json/main.go
+++ json/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"os"
 )
 
 type human struct {
@@ -85,5 +89,52 @@ func main() {
 		}
 		fmt.Println(lang)
 	}
+	// Read Full File In The Memory
+	{
+		// bytes, err := ioutil.ReadFile("./files/test.json")
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// fmt.Print(string(bytes))
+	}
 
+	{
+		// // read file in chunks
+		f, err := os.Open("./files/test.json")
+		panicOnError(err)
+		defer f.Close()
+
+		reader := bufio.NewReader(f)
+		b := make([]byte, 5)
+		for {
+			numberOfBytesRead, err := reader.Read(b)
+			if err != nil {
+				if !errors.Is(err, io.EOF) {
+					fmt.Println("Error reading file:", err)
+				}
+				break
+			}
+			fmt.Println(string(b[0:numberOfBytesRead]))
+			// fmt.Println(string(b))
+		}
+	}
+	{
+		f, err := os.Open("./files/test.json")
+		panicOnError(err)
+		defer f.Close()
+
+		s := bufio.NewScanner(f)
+		for s.Scan() {
+			fmt.Println(s.Text())
+		}
+		err = s.Err()
+		panicOnError(err)
+	}
+}
+
+func panicOnError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
